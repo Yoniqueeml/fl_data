5e040dd0d9d4f7a5088ef15d8d40869484294e45
YoniqueeZyzzFan <lolvan@mail.ru>
2022-04-16 14:27:14 +0400

    New features

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,76 +3,73 @@
 #include <string.h>
 #include <algorithm>
 #include <stdio.h>
+#include <map>
 
-//void GetMixture(const char* word, const int size, char* result, int result_counter, std::vector<const char*>& v, const int length = 1) {
-//	if (size == 0 || result_counter >= length) return;
-//	for (int i = result_counter; i <= size; ++i) {
-//		result[result_counter] = word[i]; 
-//		if (result_counter + 1 == length) {
-//			result[result_counter + 1] = 0;
-//			char* tmp = _strdup(result);
-//			v.push_back(tmp);
-//		}
-//		GetMixture(word, size-1, result, result_counter + 1, v, length);
-//	}f
-//}
-
-void GetMixture(std::vector<const char*>& results, const int size, const char* word, int length, char* Current_Res, int count = 0 ,int startIndx = 0)
+void GetMixture(std::vector<std::string>& results, std::string& word, int length, const std::string& Current_Res, int startIndx = 0)
 {
 	if (length == 0) {
-		char* x = _strdup(Current_Res);
-		x[size - count] = 0;
-		results.push_back(x);
+		results.push_back(Current_Res);
 	}
 	else {
-		for (int i = startIndx; i <= size - length; i++) {
-			Current_Res[count] = word[i];
-			GetMixture(results, size, word, length - 1, Current_Res, count + 1, i + 1);
+		for (int i = startIndx; i <= word.size() - length; i++) {
+			GetMixture(results, word, length - 1, Current_Res + word[i], i + 1);
 		}
 	}
 }
 
-void Mixture(const char* word, std::vector<std::vector<const char*>>& v, const int length = 1) {
-	std::vector<const char*> tmp;
-	int size = std::strlen(word);
-	if (length == size) { 
-		tmp.push_back(_strdup(word));
-		v.push_back(tmp);
-		return;
-	}
-	char* result = new char[size + 1];
-	result[size] = 0;
-	GetMixture(tmp, size, word, length, result,0, 0);
-	//GetMixture(word, size, 0, result, 0, tmp, length);
-	//GetMixture(word, size, result, 0, tmp, length);
+void Mixture(std::string& word, std::vector<std::vector<std::string>>& v, const int length = 1) {
+	std::vector<std::string> tmp;
+	std::string result;
+	GetMixture(tmp, word, length, result);
 	v.push_back(tmp);
-	delete[] result;
 }
 
-//int MaxSize(std::vector<std::vector<const char*>> v) {
-//	int max = std::strlen(v[0][0]);
-//	for (int i = 0; i < v.size(); ++i) {
-//		
-//	}
-//	return max;
-//}
+void FindMaxSubSequences(const double& gamma, const int& length, const std::map<std::string, int>& data) {
+	double factor = gamma * length;
+	bool check = false;
+	std::cout << "Eligible subsequences" << std::endl;
+	for (auto elem : data){
+		if (elem.second > factor) {
+			std::cout << "Sub - " << elem.first << "//  Count - " << elem.second << std::endl;
+			check = true;
+		}
+	}
+	if (check == false) {
+		std::cout << "All the subsequences failed the factor" << std::endl;
+	}
+}
 
 int main() {
-	std::vector<const char*> words;
-	words.push_back("abcd");
-	std::vector<std::vector<const char*>> all;
+	std::vector<std::string> words;
+	const int length = 2;
+	const int power = 9;
+	words.push_back("12345");
+	words.push_back("312");
+	std::vector<std::vector<std::string>> all;
+
 	for (size_t i = 0; i < words.size(); ++i) {
-		Mixture(words[i], all, 2);
+		Mixture(words[i], all, length);
 	}
-	for (auto it = all[0].begin(); it != all[0].end(); ++it) {
-		std::cout << *it << "-";
+
+	for (int i = 0; i < all.size(); i++) {
+		for (auto it = all[i].begin(); it != all[i].end(); ++it) {
+			std::cout << *it << "-";
+		}
+		std::cout << std::endl << "size:" << all[0].size() << std::endl;
 	}
-	std::cout << std::endl << "size:" << all[0].size() << std::endl;
-	//for (auto it = all[1].begin(); it != all[1].end(); ++it) {
-	//	std::cout << *it << "---";
-	//}
-	//std::cout << std::endl << "size:" << all[1].size() << std::endl;
-	// int max_size = MaxSize(all);
-	//        x  y  z
-	// void cube[][][];
+
+	std::map<std::string, int> data;
+	for (int i = 0; i < all.size(); ++i) {
+		for (int j = 0; j < all[i].size(); ++j) {
+			data[all[i][j]] += 1;
+		}
+	}
+	
+	for (auto it = data.begin(); it != data.end(); ++it) {
+		std::cout << "Sub - " << it->first<< "/// Count - " << it->second << std::endl;
+	}
+	
+	double gamma = 2;
+	FindMaxSubSequences(gamma, length, data);
+	
 }
